8724347f6cbbed5975f26c6462d31a03f5f27104
manda <manda@SHARAT>
2023-08-15 15:29:19 -0400

    Add project files.

---
+++ RPS.Data.Standard/PtInMemoryContext.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using RPS.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace RPS.Data
+{
+    public class PtInMemoryContext
+    {
+        string resourceNameItems = "RPS.Data.Standard.GenData.fs-items.json";
+        string resourceNameUsers = "RPS.Data.Standard.GenData.fs-users.json";
+
+        List<PtItem> items;
+        List<PtUser> users;
+
+        public List<PtItem> PtItems { get { return items; } }
+        public List<PtUser> PtUsers { get { return users; } }
+
+        public PtInMemoryContext()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            string contentsItems = "[]";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceNameItems))
+            using (StreamReader file = new StreamReader(stream))
+            {
+                contentsItems = file.ReadToEnd();
+            }
+
+            items = JsonConvert.DeserializeObject<List<PtItem>>(contentsItems);
+            ModifyItemDates(items);
+
+            string contentsUsers = "[]";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceNameUsers))
+            using (StreamReader file = new StreamReader(stream))
+            {
+                contentsUsers = file.ReadToEnd();
+            }
+
+            users = JsonConvert.DeserializeObject<List<PtUser>>(contentsUsers);
+        }
+
+        private void ModifyItemDates(List<PtItem> items)
+        {
+            var startDate = DateTime.Now.AddYears(-1);
+            var endDate = DateTime.Now.AddDays(-1);
+            var randomTest = new Random();
+
+            TimeSpan timeSpan = endDate - startDate;
+
+
+            items.ForEach(i =>
+            {
+                TimeSpan newSpan = new TimeSpan(0, randomTest.Next(0, (int)timeSpan.TotalMinutes), 0);
+                DateTime newDate = startDate + newSpan;
+
+                i.DateCreated = newDate;
+                i.DateModified = newDate;
+            });
+        }
+    }
+}
---
+++ RPS.Web.Server/Pages/Dashboard.razor.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor.Internal;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using RPS.Core.Models;
+using RPS.Core.Models.Dto;
+using RPS.Data;
+
+namespace RPS.Web.Server.Pages
+{
+    public partial class Dashboard : ComponentBase
+    {
+        [Inject]
+        IPtDashboardRepository RpsDashRepo { get; set; }
+
+
+
+        [Parameter]
+        public int? Months { get; set; }
+
+        [Parameter]
+        public int? UserId { get; set; }
+
+
+        public DateTime? DateStart { get; set; }
+        public DateTime? DateEnd { get; set; }
+
+        public int IssueCountOpen { get; set; }
+        public int IssueCountClosed { get; set; }
+
+        public int IssueCountActive { get { return IssueCountOpen + IssueCountClosed; } }
+        public decimal IssueCloseRate { get { if (IssueCountActive == 0) return 0;  return Math.Round((decimal)IssueCountClosed / (decimal)IssueCountActive * 100m, 2); } }
+
+        public PtDashboardFilter Filter { get; set; }
+
+        /* Combobox filter additions */
+        [Inject]
+        private IPtUserRepository RpsUserRepo { get; set; }
+
+
+        public int? SelectedAssigneeId
+        {
+            get
+            {
+                return UserId;
+            }
+            set
+            {
+                UserId = value.HasValue ? value : 0;
+                Months = Months.HasValue ? Months : 12;
+                NavigationManager.NavigateTo($"/dashboard/{Months}/{UserId}");
+            }
+        }
+        public List<PtUser> Assignees { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Assignees = RpsUserRepo.GetAll().ToList();
+            base.OnInitialized();
+        }
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+            Refresh();
+        }
+
+
+        private void Refresh()
+        {
+            DateTime start = Months.HasValue ? DateTime.Now.AddMonths(Months.Value * -1) : DateTime.Now.AddYears(-5);
+            DateTime end = DateTime.Now;
+
+            Filter = new PtDashboardFilter
+            {
+                DateStart = start,
+                DateEnd = end,
+                UserId = UserId.HasValue ? UserId.Value : 0
+            };
+
+            
+            var statusCounts = RpsDashRepo.GetStatusCounts(Filter);
+            IssueCountOpen = statusCounts.OpenItemsCount;
+            IssueCountClosed = statusCounts.ClosedItemsCount;
+
+            if (Months.HasValue)
+            {
+                DateStart = Filter.DateStart;
+                DateEnd = Filter.DateEnd;
+            }
+        }
+    }
+}
